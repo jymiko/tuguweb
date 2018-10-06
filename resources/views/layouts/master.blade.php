8f697c89c25719e7060b7134af44bcbc320cff2a
miko <miko@MacBook-Pro.local>
2018-10-06 22:26:03 +0700

    initial commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -58,7 +58,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="./img/man.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">
+            {{ Auth::user()->name }}
+          </a>
         </div>
       </div>
 
@@ -67,9 +69,22 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview menu-open">
-            <a href="#" class="nav-link active">
+          
+          <li class="nav-item">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-users"></i>
+              <p>Users</p>
+            </a>
+          </li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-table"></i>
               <p>
                 Starter Pages
                 <i class="right fa fa-angle-left"></i>
@@ -92,11 +107,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
-              </p>
+              <i class="nav-icon fas fa-power-off"></i>
+              <p>Logout</p>
             </a>
           </li>
         </ul>
